9cbb0edbec298046798fc768272725557d3a1057
Yacine Bensidahmed <yacinebensidahmed.off@gmail.com>
2024-03-11 12:49:26 +0100

    this is how to print msg and get string and print it in real-time '

--- boot.asm
+++ boot.asm
@@ -1,19 +1,36 @@
 [org 0x7c00]
 mov ah, 0x0e
-mov bx , varName
+mov bx, msg
 
-printString:
+dispchar:
     mov al, [bx]
     cmp al, 0
-    je end
+    je getStr
     int 0x10
     inc bx
-    jmp printString
-end:
+    jmp dispchar
 
-jmp $
+getStr:
+    mov bx, defStr
+    mov ah, 0
+    int 0x16
+    mov [bx], al
+    cmp al, 13
+    je endGet
+    mov ah, 0x0e
+    int 0x10
+    inc bx
+    jmp getStr
 
-varName:
-    db "Hello World!"
+endGet:
+    jmp $
+
+msg:
+    db "Enter a char : ", 0
+
+defStr:
+    times 13 db 0
+
+
 times 510-($-$$) db 0
 dw 0xaa55
