293bc8bf77c1563a7cb5f54d2dfb0e9b316b272b
Yacine Bensidahmed <yacinebensidahmed.off@gmail.com>
2024-03-11 11:12:15 +0100

    this is how to print a string

--- boot.asm
+++ boot.asm
@@ -1,16 +1,19 @@
-mov ah,0x0e
-mov al, 65
-int 0x10
+[org 0x7c00]
+mov ah, 0x0e
+mov bx , varName
 
-loop:
-        inc al 
-        cmp al, 'Z' + 1
-        je exit
-        int 0x10
-        jmp loop
-    
+printString:
+    mov al, [bx]
+    cmp al, 0
+    je end
+    int 0x10
+    inc bx
+    jmp printString
+end:
 
-exit:
-        jmp $
+jmp $
+
+varName:
+    db "Hello World!"
 times 510-($-$$) db 0
-db 0x55, 0xaa
+dw 0xaa55
