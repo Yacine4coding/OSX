582fb1a55463fdd6ee97557c00b577ce1f6cce69
Yacine Bensidahmed <yacinebensidahmed.off@gmail.com>
2024-03-11 12:51:24 +0100

    this how to get and print a char

--- boot.asm
+++ boot.asm
@@ -1,36 +1,35 @@
 [org 0x7c00]
+
 mov ah, 0x0e
 mov bx, msg
 
 dispchar:
     mov al, [bx]
     cmp al, 0
-    je getStr
+    je end
     int 0x10
-    inc bx
+    inc bx          ; Move to the next character in the message
     jmp dispchar
 
-getStr:
-    mov bx, defStr
-    mov ah, 0
-    int 0x16
-    mov [bx], al
-    cmp al, 13
-    je endGet
-    mov ah, 0x0e
-    int 0x10
-    inc bx
-    jmp getStr
-
-endGet:
-    jmp $
+end:
+    ; Your code here
 
 msg:
     db "Enter a char : ", 0
 
-defStr:
-    times 13 db 0
+getchar:
+    db 0
 
+mov ah, 0
+int 0x16
+mov [getchar], al ; store char
 
-times 510-($-$$) db 0
+;Print a char
+mov ah, 0x0e
+mov al, [getchar] ; Load the character from getchar
+int 0x10
+
+jmp $  ; Infinite loop to halt execution
+
+times 510 - ($ - $$) db 0
 dw 0xaa55
