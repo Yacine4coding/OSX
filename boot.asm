479320be879464775fbe1416e06ea5f7e321de24
Yacine Bensidahmed <yacinebensidahmed.off@gmail.com>
2024-04-27 08:38:47 +0100

    still learning the basics

--- boot.asm
+++ boot.asm
@@ -1,35 +1,29 @@
 [org 0x7c00]
 
-mov ah, 0x0e
-mov bx, msg
+mov bx, buffer
 
-dispchar:
+mov cx,13
+
+loop:
+    mov ah, 0
+    int 0x16
+    mov [bx], al
+
+    mov ah, 0x0e
     mov al, [bx]
-    cmp al, 0
-    je end
     int 0x10
-    inc bx          ; Move to the next character in the message
-    jmp dispchar
+    inc bx
 
-end:
-    ; Your code here
+    loop loop
 
-msg:
-    db "Enter a char : ", 0
+exit:
+    mov ah, 0x0e
+    mov al, 124
+    int 0x10
 
-getchar:
-    db 0
+buffer:
+    times 13 db 0
 
-mov ah, 0
-int 0x16
-mov [getchar], al ; store char
-
-;Print a char
-mov ah, 0x0e
-mov al, [getchar] ; Load the character from getchar
-int 0x10
-
-jmp $  ; Infinite loop to halt execution
-
-times 510 - ($ - $$) db 0
-dw 0xaa55
+jmp $
+times 510-($-$$) db 0
+db 0x55, 0xaa
