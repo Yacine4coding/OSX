605e9683b71de8fd677e6885e9702d1f500a8825
Yacine Bensidahmed <yacinebensidahmed.off@gmail.com>
2024-05-03 15:51:57 +0100

    chapter 5 : this code is checking err

--- boot.asm
+++ boot.asm
@@ -1,24 +1,80 @@
-[org 0x7c00]
+[org 0x7c00] 
+mov [diskNum], dl                 
 
-;Print msg
+; setting up the stack
 
-mov bx,msg
-mov cx,16
-mov ah,0x0e
-call PrntMsg
+xor ax, ax                          
+mov es, ax
+mov ds, ax
+mov bp, 0x8000
+mov sp, bp
 
-;Get String
-mov cx,5
-mov bx,buffer
-call GetChar
+mov bx, 0x7e00
 
-;Print string
-mov cx,5
-mov bx,buffer
+; reading the disk
 
-call PrntMsg
+mov ah, 2
+mov al, 1
+mov ch, 0
+mov dh, 0
+mov cl, 2
+mov dl, [diskNum]
+int 0x13
 
+jnc setmsgcf
+
+temp:
+mov ah, 0x0e
+mov al, 0
+
+cmp al,[0x7e00]
+jne setmsgal
+int 0x10
 jmp exit
+
+
+
+diskNum: 
+    db 0
+CharErr:
+    db "err : ",0
+CfChar:
+    db "carry flag ",0
+
+sectorChar:
+    db "wrong sector number ",0
+
+
+setmsgcf:
+    call PrntErr
+    mov bx,CfChar
+    mov cx,11
+    mov ah,0x0e
+    call PrntMsg
+    jmp temp
+
+setmsgal:
+    call Newline
+    call PrntErr
+    mov bx,sectorChar
+    mov cx,20
+    mov ah,0x0e
+    call PrntMsg
+    jmp exit
+
+
+Newline:
+    mov ah, 0x0e
+    mov al, 0x0A
+    int 0x10
+    ret
+
+PrntErr:
+    mov bx,CharErr
+    mov cx,6
+    mov ah,0x0e
+    call PrntMsg
+    ret
 
 PrntMsg:
     mov ah,0x0e
@@ -30,23 +86,8 @@ PrntMsg:
     jg PrntMsg
     ret
 
-GetChar:
-    mov ah,0
-    int 0x16
-    mov [bx],al
-    inc bx
-    dec cx
-    cmp cx,0
-    jg GetChar
-    ret
-
-msg:
-    db "Enter your Name:",0
-
-buffer:
-    times 5 db 0
-
 exit:
     jmp $
-times 510-($-$$) db 0
-db 0x55, 0xaa
+times 510-($-$$) db 0              
+dw 0xaa55
+times 512 db 'A'
